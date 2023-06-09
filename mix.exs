a0bee058e885cb0176930b9a9ab50c2c8ed3756d
Mark Harper <9407165+MarkHarper@users.noreply.github.com>
2023-06-09 11:16:07 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Readability.Mixfile do
   @moduledoc """
   """
 
-  @version "0.7.0"
+  @version "0.7.1"
   @description """
   A fork of the Readability library for extracting and curating articles.
   """
